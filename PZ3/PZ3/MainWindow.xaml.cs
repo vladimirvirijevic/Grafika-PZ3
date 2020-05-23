7b236196211ec9fb6a6084e223ece09e0906e666
vladimirvirijevic <vladanfs96@gmail.com>
2020-05-23 17:31:52 +0200

    Napravljena mapa sa zumiranjem i pomeranjem

--- PZ3/PZ3/MainWindow.xaml.cs
+++ PZ3/PZ3/MainWindow.xaml.cs
@@ -10,6 +10,7 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
+using System.Windows.Media.Media3D;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
@@ -20,11 +21,76 @@ namespace PZ3
     /// </summary>
     public partial class MainWindow : Window
     {
+        private Point start = new Point();
+        private Point diffOffset = new Point();
+        private int zoomMax = 200;
+        private int zoomCurent = 1;
+
         public MainWindow()
         {
             InitializeComponent();
-
-            var image = new Image();
         }
+
+        
+
+        #region test
+
+        #endregion
+
+
+
+        #region camera
+        private void viewport1_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            viewport1.CaptureMouse();
+            start = e.GetPosition(this);
+            diffOffset.X = translacija.OffsetX;
+            diffOffset.Y = translacija.OffsetY;
+        }
+
+        private void viewport1_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+        {
+            viewport1.ReleaseMouseCapture();
+        }
+
+        private void viewport1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (viewport1.IsMouseCaptured)
+            {
+                Point end = e.GetPosition(this);
+                double offsetX = (end.X - start.X) * 800;
+                double offsetY = (end.Y - start.Y) * 800;
+                double w = this.Width;
+                double h = this.Height;
+                double translateX = (offsetX * 100) / w;
+                double translateY = -(offsetY * 100) / h;
+                translacija.OffsetX = diffOffset.X + (translateX / (100 * skaliranje.ScaleX));
+                translacija.OffsetY = diffOffset.Y + (translateY / (100 * skaliranje.ScaleX));
+            }
+        }
+
+        private void viewport1_MouseWheel(object sender, MouseWheelEventArgs e)
+        {
+            Point p = e.MouseDevice.GetPosition(this);
+            double scaleX = 4;
+            double scaleY = 4;
+            if (e.Delta > 0 && zoomCurent < zoomMax)
+            {
+                scaleX = skaliranje.ScaleX + 0.1;
+                scaleY = skaliranje.ScaleY + 0.1;
+                zoomCurent++;
+                skaliranje.ScaleX = scaleX;
+                skaliranje.ScaleY = scaleY;
+            }
+            else if (e.Delta <= 0 && zoomCurent > -zoomMax)
+            {
+                scaleX = skaliranje.ScaleX - 0.1;
+                scaleY = skaliranje.ScaleY - 0.1;
+                zoomCurent--;
+                skaliranje.ScaleX = scaleX;
+                skaliranje.ScaleY = scaleY;
+            }
+        }
+        #endregion
     }
 }
